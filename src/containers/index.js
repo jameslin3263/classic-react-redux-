ca1dd383bf393f1bc397b2dec20682c45d821755
jameslin3263 <35160206+jameslin3263@users.noreply.github.com>
2018-01-19 21:55:58 +0800

    first version

---
+++ src/containers/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './index.css'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+}
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+import App from './components/App';
+
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+registerServiceWorker();
